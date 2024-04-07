dd803663149f701527ac2ebcb5384128eea27178
Serge-In <aa11@mail.ru>
2024-04-08 01:03:28 +0300

    1. Динамический список на массиве. [#158]

---
+++ src/main/java/ru/job4j/simplearraylist/SimpleArrayList.java
@@ -0,0 +1,116 @@
+package ru.job4j.simplearraylist;
+
+import java.util.*;
+
+/**
+ * 2024-04-07
+ * 1. Динамический список на массиве. [#158]
+ * В этом задании мы создадим динамический массив, аналог ArrayList.
+ * Напомню, что в Java все массивы статические, то есть при создании массива указывается его размер, который потом нельзя изменить.
+ * По сути массив - это группа ячеек памяти расположение друг за другом.
+ * Если элементов становится больше чем размер массива,
+ * то нам нужно создать новый массив большего размера и скопировать туда элементы из предыдущего массива.
+ * По такому образу работает ArrayList.
+ * 1. Внутри контейнер должен базироваться на массиве Object[] container.
+ * 2. Использовать стандартные коллекции JDK (ArrayList, LinkedList и т.д.) запрещено.
+ * 3. Контейнер должен быть динамическим, т.е. при полном заполнении увеличиваться.
+ * Увеличивать надо массив в два раза, потому что расширение это тяжелая операция.
+ * Расширение массива вынесите в отдельный метод.
+ * 4. Итератор должен реализовывать fail-fast поведение, т.е. если с момента создания итератора в коллекцию добавили новый элемент,
+ * итератор должен кидать ConcurrentModificationException. Это достигается через введение счетчика изменений - modCount.
+ * Каждая операция, которая структурно модифицирует (добавление и удаление элементов) коллекцию должна увеличивать этот счетчик.
+ * В свою очередь итератор запоминает значение этого счетчика на момент своего создания (expectedModCount),
+ * а затем на каждой итерации сравнивает сохраненное значение, с текущим значением поля modCount, если они отличаются, то генерируется исключение
+ */
+public class SimpleArrayList<T> implements SimpleList<T> {
+
+    private T[] container;
+    private int size;
+    private int modCount;
+
+    public SimpleArrayList(int capacity) {
+        this.container = (T[]) new Object[capacity];
+        this.size = 0;
+        this.modCount = 0;
+    }
+
+    @Override
+    public void add(T value) {
+        if (size == container.length){
+            this.capacityUp();
+        }
+        container[size++] = value;
+        modCount++;
+    }
+
+    public void capacityUp() {
+        int newCapacity = container.length == 0 ? 3 : container.length * 2;
+        T[] temp = container.clone();
+        container = (T[]) new Object[newCapacity];
+
+        for (int i = 0; i < temp.length; i++) {
+            container[i] = temp[i];
+        }
+    }
+
+    @Override
+    public T set(int index, T newValue) {
+        Objects.checkIndex(index, this.size);
+
+            T oldValue = container[index];
+            container[index] = newValue;
+            return oldValue;
+    }
+
+    @Override
+    public T remove(int index) {
+        Objects.checkIndex(index, this.size);
+
+        T removed = container[index];
+
+        int newCapacity = Math.max(container.length - 1, 3);
+
+        T[] temp = (T[]) new Object[newCapacity];
+        System.arraycopy(container, 0, temp, 0, index);
+        System.arraycopy(container, index + 1, temp, index ,container.length - index - 1);
+
+        container = temp;
+        size--;
+        modCount++;
+        return removed;
+    }
+
+    @Override
+    public T get(int index) {
+        Objects.checkIndex(index, this.size);
+        return container[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            int expectedModCount = modCount;
+            int current = 0;
+            @Override
+            public boolean hasNext() {
+                if (modCount != expectedModCount) {
+                    throw new ConcurrentModificationException();
+                }
+                return size != 0 && current < container.length;
+            }
+
+            @Override
+            public T next() {
+                if (hasNext()) {
+                    return container[current++];
+                }
+                throw new NoSuchElementException();
+            }
+        };
+    }
+}
---
+++ src/main/java/ru/job4j/simplearraylist/SimpleList.java
@@ -0,0 +1,13 @@
+package ru.job4j.simplearraylist;
+
+/**
+ *
+ * @param <T>
+ */
+public interface SimpleList<T> extends Iterable<T> {
+    void add(T value);
+    T set(int index, T newValue);
+    T remove(int index);
+    T get(int index);
+    int size();
+}
