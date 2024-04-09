c78ad045a78adc780c5d2d0796ed86120688201f
Serge-In <aa11@mail.ru>
2024-04-09 20:56:40 +0300

    2. Профилировщик Visual VM [#504934 #490632]

---
+++ src/main/java/ru/job4j/memory/GCTypeDemo.java
@@ -0,0 +1,40 @@
+package ru.job4j.memory;
+
+import java.util.Random;
+
+/**
+ * 2024-04-09
+ * 1. Виды сборщиков мусора [#6852]
+ */
+public class GCTypeDemo {
+
+    private static final long KB = 1000;
+    private static final long MB = KB * KB;
+    private static final Runtime ENVIRONMENT = Runtime.getRuntime();
+
+    public static void info() {
+        final long freeMemory = ENVIRONMENT.freeMemory();
+        final long totalMemory = ENVIRONMENT.totalMemory();
+        final long maxMemory = ENVIRONMENT.maxMemory();
+        System.out.println("=== Environment state ===");
+        System.out.printf("Free: %d%n", freeMemory / MB);
+        System.out.printf("Total: %d%n", totalMemory / MB);
+        System.out.printf("Max: %d%n", maxMemory / MB);
+    }
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        int length = 100;
+        info();
+
+        String[] data = new String[1_000_000];
+
+        info();
+
+        for (int i = 0; ; i = (i + 1) % data.length) {
+            data[i] = String.valueOf(
+                    (char) random.nextInt(255)
+            ).repeat(length);
+        }
+    }
+}
